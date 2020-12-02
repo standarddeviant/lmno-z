b89edbe2164a28278c8690d3bb390f79b5de3255
Dave Crist <dave.crist@gmail.com>
2020-12-01 21:30:17 -0500

    using ArduinoMenu library, functioning compile

--- src/display_interface.cpp
+++ src/display_interface.cpp
@@ -1,34 +1,165 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <IntervalTimer.h>
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <menu.h>
+#include <menuIO/serialIn.h>
+#include <menuIO/serialOut.h>
+#include <menuIO/adafruitGfxOut.h>
+#include <menuIO/encoderIn.h>
+#include <menuIO/keyIn.h>
+#include <menuIO/chainStream.h>
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 gfx(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LEDPIN A3
+
+// rotary encoder pins
+#define encA    2
+#define encB    3
+#define encBtn  4
+
+result showEvent(eventMask e,navNode& nav,prompt& item) {
+    Serial.print(F("event:"));
+    Serial.print(e);
+    return proceed;
+}
+
+int test=55;
+int ledCtrl=LOW;
+
+result myLedOn() {
+    ledCtrl=HIGH;
+    return proceed;
+}
+result myLedOff() {
+    ledCtrl=LOW;
+    return proceed;
+}
+
+result alert(menuOut& o,idleEvent e);
+result doAlert(eventMask e, prompt &item);
+
+TOGGLE(ledCtrl,setLed,"Led: ",doNothing,noEvent,noStyle//,doExit,enterEvent,noStyle
+    ,VALUE("On",HIGH,doNothing,noEvent)
+    ,VALUE("Off",LOW,doNothing,noEvent)
+);
+
+const char* constMEM hexDigit MEMMODE="0123456789ABCDEF";
+const char* constMEM hexNr[] MEMMODE={"0","x",hexDigit,hexDigit};
+char buf1[]="0x11";
+
+MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
+    ,FIELD(test,"Test","%",0,100,10,1,doNothing,noEvent,wrapStyle)
+    ,SUBMENU(setLed)
+    ,OP("LED On",myLedOn,enterEvent)
+    ,OP("LED Off",myLedOff,enterEvent)
+    ,OP("Alert test",doAlert,enterEvent)
+    ,EDIT("Hex",buf1,hexNr,doNothing,noEvent,noStyle)
+    ,EXIT("<Back")
+);
+
+// define menu colors --------------------------------------------------------
+//  {{disabled normal,disabled selected},{enabled normal,enabled selected, enabled editing}}
+//monochromatic color table
+const colorDef<uint16_t> colors[6] MEMMODE={
+    {{WHITE,BLACK},{WHITE,BLACK,BLACK}},//bgColor
+    {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//fgColor
+    {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//valColor
+    {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//unitColor
+    {{BLACK,WHITE},{WHITE,WHITE,WHITE}},//cursorColor
+    {{BLACK,WHITE},{WHITE,BLACK,BLACK}},//titleColor
+};
+
+#define gfxWidth 84
+#define gfxHeight 48
+#define fontX 6
+//5
+#define fontY 9
+#define MAX_DEPTH 2
+
+encoderIn<encA,encB> encoder;//simple quad encoder driver
+encoderInStream<encA,encB> encStream(encoder,4);// simple quad encoder fake Stream
+
+//a keyboard with only one key as the encoder button
+keyMap encBtn_map[]={{-encBtn,defaultNavCodes[enterCmd].ch}};//negative pin numbers use internal pull-up, this is on when low
+keyIn<1> encButton(encBtn_map);//1 is the number of keys
+
+serialIn serial(Serial);
+MENU_INPUTS(in,&encStream,&encButton,&serial);
+
+#define MAX_DEPTH 2
+#define textScale 1
+MENU_OUTPUTS(out,MAX_DEPTH
+    ,ADAGFX_OUT(gfx,colors,fontX,fontY,{0,0,gfxWidth/fontX,gfxHeight/fontY})
+    ,SERIAL_OUT(Serial)
+);
+
+NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
+
+result alert(menuOut& o,idleEvent e) {
+    if (e==idling) {
+        o.setCursor(0,0);
+        o.print(F("alert test"));
+        o.setCursor(0,1);
+        o.print(F("press [select]"));
+        o.setCursor(0,2);
+        o.print(F("to continue..."));
+    }
+    return proceed;
+}
+
+result doAlert(eventMask e, prompt &item) {
+    nav.idleOn(alert);
+    return proceed;
+}
+
+//when menu is suspended
+result idle(menuOut& o,idleEvent e) {
+    o.setCursor(0,0);
+    o.print(F("suspended..."));
+    o.setCursor(0,1);
+    o.print(F("press [select]"));
+    o.setCursor(0,2);
+    o.print(F("to continue"));
+    return proceed;
+}
 
 void setup_display_interface(void) {
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-        Serial.println(F("SSD1306 allocation failed"));
-        for(;;); // Don't proceed, loop forever
-    }
+    pinMode(LEDPIN,OUTPUT);
+    Serial.begin(115200);
+    while(!Serial);
+    Serial.println(F("menu 4.x test"));
+    Serial.flush();
+    nav.idleTask=idle;//point a function to be used when menu is suspended
 
-    display.clearDisplay();
+    encButton.begin();
+    encoder.begin();
 
-    display.setTextSize(1);      // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.cp437(true);         // Use full 256 char 'Code Page 437' font
-
-    // Not all the characters will fit on the display. This is normal.
-    // Library will draw what it can and the rest will be clipped.
-    // display.setCursor(x, y);     // Start at top-left corner
-    // display.printf("%s\n", s);
-
-    // display.display();
-    // delay(2000);
+    SPI.begin();
+    gfx.begin();
+    gfx.clearDisplay();
+    gfx.println(F("Menu 4.x test on GFX"));
+    // gfx.setContrast(50);
+    gfx.display(); // show splashscreen
+    delay(2000);
+    gfx.clearDisplay();
+    gfx.display(); // show splashscreen
+    // gfx.drawRect(0, 0, 84, 48, 1);
 }
+
+void update_display_interface(void) {
+    nav.doInput();
+    if(nav.changed(0)) {
+        nav.doOutput();
+        gfx.display();
+    }
+}
+
